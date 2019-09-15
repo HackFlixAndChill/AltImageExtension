18024900332cefeb0255809c33e2a07085a1a797
Zack Holbrook <zackh@mit.edu>
2019-09-15 01:48:36 -0400

    stuff

--- Access4All/imagealt.js
+++ Access4All/imagealt.js
@@ -1,14 +1,16 @@
-chrome.runtime.onMessage.addListener( function (request, sender, sendResponse) {
-	if (request.message === "images") {
-		var images = request.images;
-		// get all image tags; img.src is the key in images
-		var elements = document.getElementsByTagName("img");
-		for (var img of elements) {
-			// first check if backend returned an image alt text
-			if (img.src in images) {
-				// value is the new alt text
-				img.alt = images[img.src];
+window.onload = function () {
+	chrome.runtime.onMessage.addListener( function (request, sender, sendResponse) {
+		if (request.message === "images") {
+			var images = request.images;
+			// get all image tags; img.src is the key in images
+			var elements = document.getElementsByTagName("img");
+			for (var img of elements) {
+				// first check if backend returned an image alt text
+				if (img.src in images) {
+					// value is the new alt text
+					img.alt = images[img.src];
+				}
 			}
 		}
-	}
-});
+	});
+}
