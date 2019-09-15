32e4279e65d6915838020e7dd03959ba89324327
kylerlittle <kyler.little@wsu.edu>
2019-09-14 20:37:09 -0400

    it works

--- Access4All/background.js
+++ Access4All/background.js
@@ -1,32 +1,48 @@
-function getAltText(URL) {
-   var data = {
-       'URL': URL
-   };
-   var AWSAPIGatewayKey='AESh5ga5gU4CKGzyFpBQkSxwCuIXi6R85PLgitMg'
-   var AWSAPIGatewayBase='oq2hish015.execute-api.us-east-2.amazonaws.com'
+// global.fetch = require("node-fetch");
 
-   return fetch('https://' + AWSAPIGatewayBase + '/default/handle_accessibility', {
-       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-       headers: {
-           'Accept': 'application/json',
-           'Content-Type': 'application/json',
-           'x-api-key': AWSAPIGatewayKey,
-           'Host': AWSAPIGatewayBase
-       },
-       redirect: 'follow', // manual, *follow, error
-       referrer: 'no-referrer', // no-referrer, *client
-       body: JSON.stringify(data), // body data type must match "Content-Type" heade
-   })
-   .then(response => response.json())
-   .then(json => console.log(json));
+function getAltText(URL) {
+  var data = {
+    'URL': URL
+  };
+  var AWSAPIGatewayKey = 'AESh5ga5gU4CKGzyFpBQkSxwCuIXi6R85PLgitMg'
+  var AWSAPIGatewayBase = 'oq2hish015.execute-api.us-east-2.amazonaws.com'
+
+  return fetch('https://' + AWSAPIGatewayBase + '/default/handle_accessibility', {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'x-api-key': AWSAPIGatewayKey,
+      'Host': AWSAPIGatewayBase
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data), // body data type must match "Content-Type" heade
+  })
+    .then(response => {
+      return response.json()
+    })
+    .then(json => {
+      console.log("JSON:", json);
+      return json;
+    });
 }
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "post_url" ) {
+  function (request, sender, sendResponse) {
+    if (request.message === "post_url") {
       var url = sender.tab.url;
-      images = getAltText(url);
-      sendResponse({images: images});
+
+      getAltText(url).then((json) => {
+        images = json['alt_img_tags']
+        console.log("Images:", images);
+
+        sendResponse({ images: images });
+      });
+
+      return true;
     }
   }
 );
+
+// console.log(getAltText("https://moz.com/learn/seo/alt-text"));
--- Access4All/imagealt.js
+++ Access4All/imagealt.js
@@ -1,10 +1,17 @@
-window.onload = function() {
-	chrome.runtime.sendMessage({message: "post_url"}, function(response) {
-	  var images = response.images;
-		var elements = document.getElementById("img").src;
-		for (var img in images) {
-			
-		}
+window.onload = function () {
+	chrome.runtime.sendMessage({ message: "post_url" }, function (response) {
+		// dict of form {<imgURL: string> : <altText: string>}
+		var images = response.images;
 
+		// get all image tags; img.src is the key in images
+		var elements = document.getElementsByTagName("img");
+
+		for (var img of elements) {
+			// first check if backend returned an image alt text
+			if (img.src in images) {
+				// value is the new alt text
+				img.alt = images[img.src];
+			}
+		}
 	});
 }
